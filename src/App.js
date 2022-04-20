eed45e812d609881214c38a37d8e3fbe2f590181
Cliff Jones Jr <cliffjones@gmail.com>
2022-04-20 13:05:34 -0500

    Implement Markdown parsing.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+
 import './App.css';
 
+import logo from './logo.svg';
+import welcomePath from './welcome.md';
+
 function App() {
+  const [mdContent, setMdContent] = useState('');
+
+  fetch(welcomePath)
+    .then((response) => response.text())
+    .then((content) => setMdContent(content));
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is only a shell app. Visit <a href="https://cliffjonesjr.com">CliffJonesJr.com</a> for more.
-        </p>
+        <ReactMarkdown children={mdContent} remarkPlugins={[remarkGfm]} />
       </header>
     </div>
   );
