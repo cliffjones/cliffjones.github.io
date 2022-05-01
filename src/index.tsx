dd85d6ce31a365a310ef01248704b0a6b59ad6b8
Cliff Jones Jr <cliffjones@gmail.com>
2022-05-01 00:23:06 -0500

    Trying to get routing working with GitHub Pages.

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+// import reportWebVitals from './report-web-vitals';
 
 import './index.scss';
 import App from './app';
-import reportWebVitals from './report-web-vitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
 
 // Report analytics. Learn more at `https://bit.ly/CRA-vitals`.
-reportWebVitals(console.log);
+// TODO: https://www.simoahava.com/analytics/track-core-web-vitals-in-ga4-with-google-tag-manager/
+// reportWebVitals(console.log);
