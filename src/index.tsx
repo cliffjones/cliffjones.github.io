91b7287ecb092ea5c945cac08c1582859b910780
Cliff Jones Jr <cliffjones@gmail.com>
2022-04-30 20:16:12 -0500

    Trying to get routing working with GitHub Pages.

--- src/index.tsx
+++ src/index.tsx
@@ -9,13 +9,11 @@ import reportWebVitals from './report-web-vitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Report analytics. Learn more at `https://bit.ly/CRA-vitals`.
+reportWebVitals(console.log);
